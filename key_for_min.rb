bdaa24609a5ec7c8e16243628dead520b526740f
Zackary Cammack <zack.cammack@gmail.com>
2020-08-22 15:03:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_key = name_hash.reduce do |key, value|
+  lowest_key = name_hash.inject do |key, value|
     key.last > value.last ? value : key
   end
     if name_hash == {}
